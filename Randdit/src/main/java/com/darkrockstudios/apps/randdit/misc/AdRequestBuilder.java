c22e3283630c3dd78de09a657de65b45278db2e1
Adam Brown <adamwbrown@gmail.com>
2013-12-10 20:50:40 -0800

    - Added DRS on my TF201 to the test devices for Ads

--- Randdit/src/main/java/com/darkrockstudios/apps/randdit/misc/AdRequestBuilder.java
+++ Randdit/src/main/java/com/darkrockstudios/apps/randdit/misc/AdRequestBuilder.java
@@ -11,8 +11,9 @@ public class AdRequestBuilder
 	{
 		AdRequest.Builder adBuilder = new AdRequest.Builder();
 		adBuilder.addTestDevice( AdRequest.DEVICE_ID_EMULATOR );
-		adBuilder.addTestDevice( "CAA9C926FB13FA7FEE6E98B745B46A39" );
-		adBuilder.addTestDevice( "ADD12FD43C2481D9F9BACE38F280D31B" );
+		adBuilder.addTestDevice( "CAA9C926FB13FA7FEE6E98B745B46A39" ); // My GN
+		adBuilder.addTestDevice( "ADD12FD43C2481D9F9BACE38F280D31B" ); // MY TF201, Me
+		adBuilder.addTestDevice( "36CDCE2BA7F710EB8E8F68E3A46F780C" ); // My TF201, DRS
 		adBuilder.tagForChildDirectedTreatment( false );
 
 		return adBuilder.build();
